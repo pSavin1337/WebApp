3a08ca2fed48d4771a18508dc9289327473de0e9
pSavin1337 <p.savin73@gmail.com>
2022-02-27 16:48:54 +0400

    added showing page name function

--- app/src/main/java/com/lospollos/webapp/view/fragments/WebFragment.kt
+++ app/src/main/java/com/lospollos/webapp/view/fragments/WebFragment.kt
@@ -1,5 +1,6 @@
 package com.lospollos.webapp.view.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,10 +8,46 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import com.lospollos.webapp.R
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+import android.webkit.WebResourceRequest
+
+import android.os.Build
+
+import android.annotation.TargetApi
+import com.lospollos.webapp.view.activity.MainActivity
+
 
 class WebFragment : Fragment() {
 
-    lateinit var webView: WebView
+    private lateinit var webView: WebView
+    val bundle = Bundle()
+
+    private fun getPageName(url: String): String {
+        val urlArr = url.split('/')
+        return urlArr[urlArr.size - 1].split('.')[0]
+    }
+
+    private var webViewClient: WebViewClient = object : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            bundle.putString("page_name", getPageName(url))
+            (activity as MainActivity).navController.navigate(
+                R.id.action_webFragment_to_webInfoFragment,
+                bundle
+            )
+            return true
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+            bundle.putString("page_name", getPageName(request.url.toString()))
+            (activity as MainActivity).navController.navigate(
+                R.id.action_webFragment_to_webInfoFragment,
+                bundle
+            )
+            return true
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,10 +56,14 @@ class WebFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_web, container, false)
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         webView = view.findViewById(R.id.web_view)
         webView.loadUrl("https://startandroid.ru/ru/uroki/vse-uroki-spiskom.html")
+        webView.webViewClient = webViewClient
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
     }
 
 }
--- app/src/main/java/com/lospollos/webapp/view/fragments/WebInfoFragment.kt
+++ app/src/main/java/com/lospollos/webapp/view/fragments/WebInfoFragment.kt
@@ -5,18 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.lospollos.webapp.R
 
 class WebInfoFragment : Fragment() {
+
+    private lateinit var infoTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_web_info, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        infoTextView = view.findViewById(R.id.info_text_view)
+        infoTextView.text = arguments?.getString("page_name")
+    }
 
 }
